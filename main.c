9264458802bff119dfb6506d80c4c09b4e9071ab
TESTMECS <axwar7410@gmail.com>
2026-03-08 15:42:56 -0400

    ok workable

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 960
 #define MAX_INPUT_CHARS 256
 
 typedef struct {
-  int tomatoes;
+  int laps;
   int peice;
-  int smallest;
-  int largest;
+  uint smallest;
+  uint largest;
   int state;
   int input_step;
   int pomodoro_state;
@@ -51,7 +51,7 @@ int main(void) {
   P.lapC = 0;
   P.is_paused = false;
   strcpy(P.input, "");
-  P.prompt = "Please input your loop";
+  P.prompt = "Please input your pomodoro loop";
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pomodoro Timer");
@@ -76,9 +76,9 @@ int main(void) {
     }
     case 2: {
       BeginDrawing();
-      ClearBackground(RAYWHITE);
-      DrawText("Congrats", (GetScreenWidth() / 2.0f) - 10,
-               GetScreenHeight() * 0.5f, 50, RED);
+      ClearBackground(DARKBLUE);
+      DrawText("Congrats", (GetScreenWidth() / 1.5f) - 10,
+               GetScreenHeight() * 0.8f, 50, RED);
       EndDrawing();
       break;
     }
@@ -106,6 +106,9 @@ int make_it_seconds(int input[]) {
   int h = input[0];
   int m = input[1];
   int s = input[2];
+  if (m == 0 && s == 0) {
+    return h * 60;
+  }
   return h * 3600 + m * 60 + s;
 }
 
@@ -115,20 +118,21 @@ void get_user_input() {
   static int *biggest_time = NULL;
 
   BeginDrawing();
-  ClearBackground(RAYWHITE);
+  ClearBackground(DARKGRAY);
   GuiGetStyle(DEFAULT, BACKGROUND_COLOR);
 
   Rectangle bounds = {(GetScreenWidth() / 2.0f) - (255 / 2),
                       GetScreenHeight() * 0.45f, 255, 255};
 
-  int result = GuiTextInputBox(bounds, "Input", P.prompt, "Ok;Cancel", P.input, 255, NULL);
+  int result = GuiTextInputBox(bounds, "Input", P.prompt, "Ok;Cancel", P.input,
+                               255, NULL);
 
   if (result == 1) {
     switch (P.input_step) {
     case 0: {
-      P.tomatoes = atoi(P.input);
+      P.laps = atoi(P.input);
       P.input_step++;
-      P.prompt = "Please input lap duration";
+      P.prompt = "Pomodoro duration (minutes or mm:ss)";
       P.input[0] = '\0';
       break;
     }
@@ -136,7 +140,7 @@ void get_user_input() {
       peice_time = make_it_int(P.input);
       P.peice = make_it_seconds(peice_time);
       P.input_step++;
-      P.prompt = "Please input your short break duration";
+      P.prompt = "Short break (minutes or mm:ss)";
       P.input[0] = '\0';
       break;
     }
@@ -144,7 +148,7 @@ void get_user_input() {
       smallest_time = make_it_int(P.input);
       P.smallest = make_it_seconds(smallest_time);
       P.input_step++;
-      P.prompt = "Please input your long break duration";
+      P.prompt = "Long break (minutes or mm:ss)";
       P.input[0] = '\0';
       break;
     }
@@ -164,17 +168,25 @@ void get_user_input() {
 
 void pomodoro() {
   static char icon[16] = "#132#";
+  static double last_time = 0;
 
   if (P.loopC == 0 && P.lapC == 0 && P.countdown == 0) {
-    P.countdown = P.peice * 60;
+    P.countdown = P.peice;
+    last_time = GetTime();
+  }
+
+  double current_time = GetTime();
+  if (current_time - last_time >= 1.0) {
+    P.countdown--;
+    last_time = current_time;
   }
 
   BeginDrawing();
-  ClearBackground(RAYWHITE);
+  ClearBackground(DARKGRAY);
   GuiGetStyle(DEFAULT, BACKGROUND_COLOR);
 
-  GuiToggle((Rectangle){(GetScreenWidth() / 2.0f) - 25,
-                        GetScreenHeight() * 0.8, 50, 50},
+  GuiToggle((Rectangle){(GetScreenWidth() / 2.0f) - 25, GetScreenHeight() * 0.8,
+                        50, 50},
             icon, &P.is_paused);
 
   if (P.is_paused) {
@@ -185,58 +197,60 @@ void pomodoro() {
     if (P.countdown >= 0) {
       switch (P.pomodoro_state) {
       case 0: {
-        DrawText(TextFormat("Tomato %i/%i, Peice %i/4", P.loopC + 1, P.tomatoes,
+        DrawText(TextFormat("Lap %i/%i, Peice %i/4", P.loopC + 1, P.laps,
                             P.lapC + 1),
-                 (GetScreenWidth() / 2.0f) - 100, GetScreenHeight() * 0.3f,
-                 20, RED);
-        DrawText(TextFormat("%i sec left...", P.countdown / 60),
-                 (GetScreenWidth() / 2.0f) - 50, GetScreenHeight() * 0.5f, 20,
-                 ORANGE);
+                 (GetScreenWidth() / 2.0f) - 150, GetScreenHeight() * 0.3f, 30,
+                 RED);
+        DrawText(
+            TextFormat("%02d:%02d left...", P.countdown / 60, P.countdown % 60),
+            (GetScreenWidth() / 2.0f) - 100, GetScreenHeight() * 0.5f, 60,
+            ORANGE);
         break;
       }
       case 1: {
-        DrawText("Start small rest...", (GetScreenWidth() / 2.0f) - 20,
-                 GetScreenHeight() * 0.3f, 20, RED);
-        DrawText(TextFormat("%i sec left...", P.countdown / 60),
-                 (GetScreenWidth() / 2.0f) - 50, GetScreenHeight() * 0.5f, 20,
-                 ORANGE);
+        DrawText("Start small rest...", (GetScreenWidth() / 2.0f) - 50,
+                 GetScreenHeight() * 0.3f, 30, RED);
+        DrawText(
+            TextFormat("%02d:%02d left...", P.countdown / 60, P.countdown % 60),
+            (GetScreenWidth() / 2.0f) - 100, GetScreenHeight() * 0.5f, 60,
+            ORANGE);
         break;
       }
       case 2: {
-        DrawText("Start long rest...", (GetScreenWidth() / 2.0f) - 20,
-                 GetScreenHeight() * 0.3f, 20, RED);
-        DrawText(TextFormat("%i sec left...", P.countdown / 60),
-                 (GetScreenWidth() / 2.0f) - 50, GetScreenHeight() * 0.5f, 20,
-                 ORANGE);
+        DrawText("Start long rest...", (GetScreenWidth() / 2.0f) - 50,
+                 GetScreenHeight() * 0.3f, 30, RED);
+        DrawText(
+            TextFormat("%02d:%02d left...", P.countdown / 60, P.countdown % 60),
+            (GetScreenWidth() / 2.0f) - 100, GetScreenHeight() * 0.5f, 60,
+            ORANGE);
         break;
       }
       }
-      P.countdown--;
     } else {
       switch (P.pomodoro_state) {
       case 0: {
         if (P.lapC < 3) {
           P.lapC++;
-          P.countdown = P.smallest * 60;
+          P.countdown = P.smallest;
           P.pomodoro_state = 1;
         } else {
           P.lapC = 0;
           P.loopC++;
-          P.countdown = P.largest * 60;
+          P.countdown = P.largest;
           P.pomodoro_state = 2;
         }
-        if (P.loopC >= P.tomatoes) {
+        if (P.loopC >= P.laps) {
           P.pomodoro_done = true;
         }
         break;
       }
       case 1: {
-        P.countdown = P.peice * 60;
+        P.countdown = P.peice;
         P.pomodoro_state = 0;
         break;
       }
       case 2: {
-        P.countdown = P.peice * 60;
+        P.countdown = P.peice;
         P.pomodoro_state = 0;
         break;
       }
